f6ec010a7581042f4c0bc85eb6a4eee0a147741e
Rizwan Ahsan <rizahsan4@gmail.com>
2020-06-16 14:08:27 -0400

    saving progress

--- main.py
+++ main.py
@@ -1,105 +1,131 @@
 import sqlite3
-import bcrypt
 from cryptography.fernet import Fernet
+import cryptography
 import random
 import string
 
 conn = sqlite3.connect("info.db")
 cur = conn.cursor()
+key = None
 
-class Database():
-    def __init__(self, username, master_password):
-        self.username = username
-        self.master_password = master_password
+def storeUser_MasterPass(username):
+    cur.execute(f"INSERT INTO info (username) VALUES ('{username}');")
+    conn.commit()
 
-    def storeUser_MasterPass(self):
-        hashedMasterPass = bcrypt.hashpw(self.master_password.encode("utf-8"), bcrypt.gensalt())
-        insertQuery = "INSERT INTO info (username, master_password) VALUES (?, ?);"
-        cur.execute(insertQuery, (self.username, hashedMasterPass))
-        conn.commit()
+def addServiceAndPass(username):
+    """Store the randomly generated password in the selected service column"""
+    service = input("Enter the service name: ")
+    columns = [i[1] for i in cur.execute("PRAGMA table_info(info)")]
+    print(columns)
 
-    def addGeneratedPass(self):
-        """Store the randomly generated password in the selected service column"""
-        service = input("Enter the service name: ")
-        columns = [i[1] for i in cur.execute("PRAGMA table_info(info)")]
+    if service not in columns:
+        cur.execute(f"ALTER TABLE info ADD COLUMN '{service}' TEXT;")
+    conn.commit()
 
-        if service not in columns:
-            cur.execute("ALTER TABLE info ADD COLUMN '{}' TEXT;".format(service))
+    cur.execute(f"SELECT COUNT({service}) FROM info WHERE username = '{username}';")
+    record = cur.fetchone()[0]
 
-        cur.execute(f"SELECT COUNT({service}) FROM info WHERE username = '{self.username}';")
-        record = cur.fetchone()[0]
+    if record == 0:
+        cur.execute(f"UPDATE info SET '{service}' = '{generatePass()}' WHERE username = '{username}';")
+    conn.commit()
 
-        if record == 0:
-            cur.execute(f"UPDATE info SET '{service}' = '{self.generatePass()}' WHERE username = '{self.username}';")
-        conn.commit()
+def generatePass():
+    """
+    Generate a random 25 char password containing lowercase, uppercase, numbers and special chars
+    :return: str
+    """
+    global key
+    password = ""
+    randomSource = string.ascii_letters + string.digits + string.punctuation
 
-    def generatePass(self):
-        """
-        Generate a random 25 char password containing lowercase, uppercase, numbers and special chars
-        :return: str
-        """
-        password = ""
-        randomSource = string.ascii_letters + string.digits + string.punctuation
+    for i in range(3):
+        password += random.choice(string.ascii_lowercase)
+        password += random.choice(string.ascii_uppercase)
+        password += random.choice(string.digits)
+        password += random.choice(string.punctuation)
+        password += random.choice(string.digits)
 
-        for i in range(3):
-            password += random.choice(string.ascii_lowercase)
-            password += random.choice(string.ascii_uppercase)
-            password += random.choice(string.digits)
-            password += random.choice(string.punctuation)
-            password += random.choice(string.digits)
+    for i in range(10):
+        password += random.choice(randomSource)
 
-        for i in range(10):
-            password += random.choice(randomSource)
-
-        passwordList = list(password)
-        random.shuffle(passwordList)
-        random.shuffle(passwordList)
-        password = ''.join(passwordList)
-        return password
+    passwordList = list(password)
+    random.shuffle(passwordList)
+    random.shuffle(passwordList)
+    password = ''.join(passwordList)
+    f = Fernet(key)
+    token = f.encrypt(password.encode("utf-8"))
+    return token.decode("utf-8")
 
 def createTable():
     cur.execute("""CREATE TABLE IF NOT EXISTS info
-                    (username TEXT PRIMARY KEY NOT NULL,
-                    master_password BLOB);""")
+                    (username TEXT PRIMARY KEY NOT NULL);""")
     conn.commit()
 
-def addUser():
-    while True:
-        username = input("Set your username: ")
-        cur.execute(f"SELECT COUNT(username) FROM info WHERE username = '{username}';")
-        record = cur.fetchone()[0]
-        if record == 0:
-            break
-        print("There is already a similar username. Enter a different username.")
+def generateKey(username):
+    global key
+    key = Fernet.generate_key()
+    with open(username + "(key).txt", "w") as f:
+        f.write("*********************************************************************\n")
+        f.write("                 STORE THIS KEY IN A SECURE PLACE!\n")
+        f.write("*********************************************************************\n")
+        f.write("\n")
+        f.write(key.decode("utf-8"))
+        f.close()
 
-    MasterPass = input("Set your master password: ")
-    newUser = Database(username, MasterPass)
-    newUser.storeUser_MasterPass()
-
-    print("Your info has been stored in the database.")
-
-def fetchData():
-    cur.execute("SELECT * FROM info;")
+def fetchData(username):
+    global key
+    cur.execute(f"SELECT * FROM info WHERE username = '{username}';")
     record = cur.fetchall()
-    print(record)
+    f = Fernet(key)
     for row in record:
-        print("username:", row[0], "password:", row[1], "facebook:", row[2], "youtube:", row[3])
+        print("username:", row[0], "facebook:", row[1], "youtube:", row[2])
+        print(f.decrypt(row[1].encode("utf-8")).decode("utf-8"))
+        # print(f.decrypt(row[2].encode("utf-8")).decode("utf-8"))
 
-def checkPassword():
+
+def main():
+    global key
+    createUser = "n"
     username = input("Enter yout username: ")
-    cur.execute(f"SELECT master_password FROM info WHERE username = '{username}';")
-    record = cur.fetchall()
-    password = input("Enter your master password: ")
-    return bcrypt.checkpw(password.encode("utf-8"), record[0][0])
+    cur.execute(f"SELECT COUNT(username) FROM info WHERE username = '{username}';")
+    record = cur.fetchone()[0]
+    if record == 0:
+        while True:
+            createUser = input("Username does not exist. Do you want to create a new? Enter y or n: ")
+            if createUser == "y" or createUser == "n":
+                break
 
-user1 = Database("Rizwan", "hello")
-user2 = Database("Omar", "12345")
+        if createUser == "y":
+            storeUser_MasterPass(username)
+            generateKey(username)
+            print("Your info has been stored in the database.")
+
+    while True:
+        if createUser == "n" or record != 0:
+            key = input("Enter your master key: ").encode("utf-8")
+        answer = input("Do you want to add a new service? Enter y or n: ")
+        if answer == "y" or answer == "n":
+            break
+
+    if answer == "y":
+        print("key:", key)
+        addServiceAndPass(username)
+
 createTable()
-# user1.storeUser_MasterPass()
-# user2.storeUser_MasterPass()
 # print(user1.userKey)
-# addUser()
 # print(checkPassword())
-# user2.addGeneratedPass()
-fetchData()
+# user1.addGeneratedPass()
+# print(generatePass())
+main()
+# fetchData("Abbu")
 conn.close()
+
+# try:
+#     key = Fernet.generate_key()
+#     f = Fernet(key)
+#     token = f.encrypt(b"my deep dark secret")
+#     key = Fernet.generate_key()
+#     f = Fernet(key)
+#     print(f.decrypt(token))
+# except (cryptography.fernet.InvalidToken):
+#     print("Invalid key")
